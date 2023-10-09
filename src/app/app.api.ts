3fc31bd8238456924b788cda05654aa29fd1eced
flabiofilho <flabio.assuncao@neuraspace.com>
2023-10-09 22:36:03 +0100

    fix

--- src/app/app.api.ts
+++ src/app/app.api.ts
@@ -1,2 +1,2 @@
-export const GIRA_API = 'https://localhost:7181/';
-//export const GIRA_API = 'https://girasmanagement.fly.dev/';
+//export const GIRA_API = 'https://localhost:7181/';
+export const GIRA_API = 'https://girasmanagement.fly.dev/';
